b1cfac09493f4f1a46ab55e60d354464ffd65c42
hingashi-coder <yukito@toukoutounoMacBook-Air.local>
2020-11-09 18:21:31 +0900

    Line登録のメッセージ表示を追加。

--- src/components/authorization/authorization.vue
+++ src/components/authorization/authorization.vue
@@ -24,13 +24,18 @@ export default {
   methods: {
     authCheck () {
       this.sendAuthCode(this.checkCode).then(() => {
-        console.log('OK')
+        this.$ons.notification.alert(
+          '正常にLineと連携できました。',
+          {
+            title: '成功'
+          }
+        )
       }).catch(err => {
         console.log(err)
         this.$ons.notification.alert(
           err.toString(),
           {
-            title: 'ログイン失敗'
+            title: '認証失敗'
           }
         )
       })
@@ -42,7 +47,11 @@ export default {
             token: token,
             authCode: Number(this.checkCode)
           }).then(e => {
-            console.log(e)
+            if (e.data.status === 'OK') {
+              resolve()
+            } else {
+              reject(e.data.msg)
+            }
           })
         })
       })
