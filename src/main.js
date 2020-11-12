94d5db11c7d0b5c1aa6b4af32e353ced00368adc
k018c1069 <k018c1069@it-neec.jp>
2020-11-12 16:26:17 +0900

    PWA実装,line認証変更

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import 'onsenui/css/onsenui.css'
 import 'onsenui/css/onsen-css-components.css'
 import VueOnsen from 'vue-onsenui'
+import "./registerServiceWorker"
 
 Vue.config.productionTip = false
 Vue.use(VueOnsen)
