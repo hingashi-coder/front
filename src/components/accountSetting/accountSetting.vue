4eda21f432160fbf6771371ce98fb63cff3983dd
k018c1069 <k018c1069@it-neec.jp>
2020-11-16 15:32:48 +0900

    パスワード変更画面、メールアドレス変更画面実装

---
+++ src/components/accountSetting/accountSetting.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-ons-page>
+    <goBackBar
+      title="アカウント設定"
+      back
+    />
+    <v-ons-list>
+      <v-ons-list-header>ログインとセキュリティ</v-ons-list-header>
+      <v-ons-list-item
+        tappable
+        v-for="menu in settingMenu"
+        :key="menu.title"
+        @click="menu.clickCallback"
+      >
+      <div class="left">{{menu.title}}</div>
+      </v-ons-list-item>
+    </v-ons-list>
+  </v-ons-page>
+</template>
+
+<script>
+import goBackBar from '@/components/goBackBar/goBackBar'
+export default {
+  components: {
+    goBackBar
+  },
+  data () {
+    const self = this
+    return {
+      settingMenu: [
+        {
+          title: 'メールアドレス変更',
+          clickCallback () {
+            self.$router.push({
+              name: 'changeAddress'
+            })
+          }
+        },
+        {
+          title: 'パスワード変更',
+          clickCallback () {
+            self.$router.push('changePassword')
+          }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/accountSetting/changePass/changePassword.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-ons-page>
+    <goBackBar
+      title="パスワード変更"
+      back
+    />
+    <v-ons-list>
+      <v-ons-list-item>
+        <v-ons-input placeholder='現在のパスワード' v-model="oldPass" type="password"/>
+      </v-ons-list-item>
+      <v-ons-list-item>
+        <v-ons-input placeholder='新しいパスワード' v-model="newPass"/>
+      </v-ons-list-item>
+      <v-ons-list-item>
+        <v-ons-input placeholder='新しいパスワード' v-model="newPassCheck"/>
+      </v-ons-list-item>
+    </v-ons-list>
+    <v-ons-button
+    modifier="large"
+    style="margin: 6px 0"
+    @click="changePassword"
+    >変更</v-ons-button>
+  </v-ons-page>
+</template>
+
+<script>
+import goBackBar from '@/components/goBackBar/goBackBar'
+export default {
+  components: {
+    goBackBar
+  },
+  data () {
+    return {
+      oldPass: '',
+      newPass: '',
+      newPassCheck: ''
+    }
+  },
+  methods: {
+    changePassword () {
+      if (this.oldPass === this.newPass) {
+        this.$ons.notification.alert('パスワードが同じです')
+      } else if (this.newPass === this.newPassCheck) {
+        this.$ons.notification.alert('OK')
+      } else {
+        this.$ons.notification.alert('パスワードが違います')
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
